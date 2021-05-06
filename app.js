e6f82d952f861fa82b07cee0622a404e1c106d7d
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-05-07 00:09:09 +0200

    tutorial part 1+2

--- app.js
+++ app.js
@@ -4,10 +4,19 @@ const app = Vue.createApp({
     // template: '<h2>I am the template</h2>'
     data() {
         return {
+            url: 'http://www.example.com',
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45
+            // title: 'The Final Empire',
+            // author: 'Brandon Sanderson',
+            // age: 45,
+            // isFav - dynamic class
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+                { title: 'the way of kings', author: 'Brandon Sanderson', img: 'assets/2.jpg', isFav: false},
+                { title: 'the final empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isFav: true},
+            ],
+            x: 0,
+            y: 0
         }
     },
     methods: {
@@ -16,11 +25,26 @@ const app = Vue.createApp({
             //this.title = title
             // console.log('you clicked me')
         // }
+        toogleFav(book) {
+            book.isFav = !book.isFav
+        },
         toogleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e) {
-            console.log(e)
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX,
+            this.y = e.offsetY
+        }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
         }
     }
 })
