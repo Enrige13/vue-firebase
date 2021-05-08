31861d188f21cae77a5e0df9b21fb50a343834b5
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-05-08 13:26:30 +0200

    #8 The Vue Router: Router links, Folder structure, route parameters, dynamic links, redirects

---
+++ ninja-jobs/src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sint dignissimos blanditiis vero voluptatem iste consectetur esse ullam animi nobis necessitatibus? Dolor placeat unde a nostrum obcaecati, qui eius necessitatibus cupiditate.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque laborum quidem voluptatum quis esse reprehenderit ea. Quisquam aperiam in eos ex quam. Perferendis minus minima neque, impedit similique provident repudiandae.</p>
+  </div>
+</template>
---
+++ ninja-jobs/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus aspernatur fuga, natus iure vel id harum asperiores maiores nostrum dolores. Quibusdam dolore vitae cupiditate, quis non tempore error esse quam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus aspernatur fuga, natus iure vel id harum asperiores maiores nostrum dolores. Quibusdam dolore vitae cupiditate, quis non tempore error esse quam.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
