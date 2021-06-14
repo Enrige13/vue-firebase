137017c7842e4fb3d872b4c38b5d784c909ae4e0
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-14 13:23:44 +0200

    #69: Showing Filtered Projects

--- 8-project-planner/src/views/Home.vue
+++ 8-project-planner/src/views/Home.vue
@@ -3,7 +3,7 @@
     <FilterNav @filterChange="current = $event" :current="current" /> <!-- $event - event from updateFilter -->
     <div v-if="projects.length"> 
       <!-- if array has a length, it outputs it here otherwise not -->
-      <div v-for="project in projects" :key="project.id">
+      <div v-for="project in filteredProjects" :key="project.id">
         <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
       </div>
     </div>
@@ -43,6 +43,17 @@ export default {
         return project.id === id
       })
       p.complete = !p.complete
+    }
+  },
+  computed: {
+    filteredProjects() {
+      if (this.current === 'completed') {
+        return this.projects.filter(project => project.complete)
+      }
+      if (this.current === 'ongoing') {
+        return this.projects.filter(project => !project.complete)
+      }
+      return this.projects
     }
   }
 }
