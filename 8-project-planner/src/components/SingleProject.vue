f93cd3922222a65d1d028dbcbf2b6da888dddab2
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-14 11:00:41 +0200

    #63: New Project Form

--- 8-project-planner/src/components/SingleProject.vue
+++ 8-project-planner/src/components/SingleProject.vue
@@ -1,78 +1,78 @@
 <template>
-  <div class="project" :class="{ complete: project.complete }">
-      <div class="actions">
-            <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
-            <div class="icons">
-                <span @click="deleteProject" class="material-icons">delete</span>
-                <span class="material-icons">edit</span>
-                <span @click="toggleComplete" class="material-icons tick">done</span>
-            </div>
-      </div>
-      <div v-if="showDetails" class="details">
-          <p>{{ project.details }}</p>
-      </div>
+<div class="project" :class="{ complete: project.complete }">
+  <div class="actions">
+    <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
+    <div class="icons">
+      <span @click="deleteProject" class="material-icons">delete</span>
+      <span class="material-icons">edit</span>
+      <span @click="toggleComplete" class="material-icons tick">done</span>
+    </div>
   </div>
+  <div v-if="showDetails" class="details">
+    <p>{{ project.details }}</p>
+  </div>
+</div>
 </template>
 
 <script>
 export default {
-    props: ['project'],
-    data() {
-        return {
-          showDetails: false,
-          uri: 'http://localhost:3000/projects/' + this.project.id,
-        }
+  props: ['project'],
+  data() {
+      return {
+        showDetails: false,
+        uri: 'http://localhost:3000/projects/' + this.project.id,
+      }
+  },
+  methods: {
+    deleteProject() {
+      fetch(this.uri, { method: 'DELETE' })
+        .then(() => this.$emit('delete', this.project.id))
+        .catch(err => console.log(err))
     },
-    methods: {
-        deleteProject() {
-            fetch(this.uri, { method: 'DELETE' })
-                .then(() => this.$emit('delete', this.project.id))
-                .catch(err => console.log(err))
-        },
-        toggleComplete() {
-            fetch(this.uri, { 
-                method: 'PATCH', // Patch request only for one state in the json file
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ complete: !this.project.complete }) //send json Data back and forth (need to be JSON String)
-            }).then(() => {
-                this.$emit('complete', this.project.id)
-            }).catch(err => console.log(err))
-        }
+    toggleComplete() {
+      fetch(this.uri, { 
+        method: 'PATCH', // Patch request only for one state in the json file
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ complete: !this.project.complete }) //send json Data back and forth (need to be JSON String)
+      }).then(() => {
+        this.$emit('complete', this.project.id)
+      }).catch(err => console.log(err))
     }
+  }
 }
 </script>
 
 <style scoped>
 .project {
-    margin: 20px auto;
-    background: white;
-    padding: 10px 20px;
-    border-radius: 4px;
-    box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
-    border-left: 4px solid #e90074;
+  margin: 20px auto;
+  background: white;
+  padding: 10px 20px;
+  border-radius: 4px;
+  box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
+  border-left: 4px solid #e90074;
 }
 h3 {
-    cursor: pointer;
+  cursor: pointer;
 }
 .actions {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 .material-icons {
-    font-size: 24px;
-    margin-left: 10px;
-    color: #bbb;
-    cursor: pointer;
+  font-size: 24px;
+  margin-left: 10px;
+  color: #bbb;
+  cursor: pointer;
 }
 .material-icons:hover {
-    color: #777;
+  color: #777;
 }
 /* completed projects */
 .project.complete {
-    border-left: 4px solid #00ce89;
+  border-left: 4px solid #00ce89;
 }
 .project.complete .tick {
-    color: #00ce89;
+  color: #00ce89;
 }
 </style>
