ce935e65186e2a99c08e56bac04a8ba527520564
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-22 15:30:45 +0200

    #102: Firebase Setup

---
+++ 13-live-chat/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgp4_1jccI9lUtylSctXM_c_h6wRrWHNw",
+    authDomain: "udemy-vue-firebase-sites-28250.firebaseapp.com",
+    projectId: "udemy-vue-firebase-sites-28250",
+    storageBucket: "udemy-vue-firebase-sites-28250.appspot.com",
+    messagingSenderId: "997683555963",
+    appId: "1:997683555963:web:1dfbc9a76d1f4ddb68bcde"
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
