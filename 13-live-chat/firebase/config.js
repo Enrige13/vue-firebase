9c23285599173efbe9c78c06eafa5e03da3b28ef
Enrige13 <55575601+Enrige13@users.noreply.github.com>
2021-06-22 15:53:17 +0200

    #105: Firebase Auth

--- 13-live-chat/firebase/config.js
+++ 13-live-chat/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDgp4_1jccI9lUtylSctXM_c_h6wRrWHNw",
@@ -13,7 +14,10 @@ const firebaseConfig = {
 // init firebase
 firebase.initializeApp(firebaseConfig)
 
+// Auth method
+const projectAuth = firebase.auth()
+
 const projectFirestore = firebase.firestore()
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectFirestore, timestamp }
+export { projectAuth, projectFirestore, timestamp }
